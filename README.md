cef95747c847ce686b354837636ed93f454db613
Mantas <gusmantas@gmail.com>
2020-06-03 11:04:06 +0200

    Final commit. Documentation, few optimizations and fixes.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# csv_to_db_converter
-Application for converting .csv files to local databases. 
+# CSV-to-Database Converter
+This is a simple application used to convert a .csv file to local database.
+Simply, import a file with a single button, preview its content, give a name to to-be-converted
+database, hit "Convert" button and set a location for your new database file. 
+Databases generated with this tool can be used as regular databases
+for programming.
+
+NOTE!
+
+Foreign keys are not added after converting, and must be placed manually. 
+This tool also adds an index column to database, even if .csv file have it, so make sure to review
+database before using it.
+To open database file use a database management tool, such as SQLite Studio, HeidiSQL or other. 
+
+This application was built using Python with these libraries:
+* TKinter
+* Pandas
+* SQLAlchemy
+
--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter.filedialog import askdirectory
-import pandas as pd
 from tkinter import messagebox
-from pandas.errors import EmptyDataError
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from pandas.errors import EmptyDataError
+import pandas as pd
 
 
 class Client(object):
     """
-    A class that creates a client.
-    A tkinter box that allows users to convert a csv file to a local database
+    A class that creates a client with
+    tkinter box that allows users to convert a csv file to a local database
     using sql alchemy and sqlite3 libraries.
     """
 
@@ -35,11 +35,11 @@ class Client(object):
 
         self.convert_button = Button(tk_window, text="Convert File", command=self.create_database)
         self.convert_button.grid(column=14, row=0, columnspan=2, pady=(0, 10))
-        # Button state set to normal after csv file is chosen and can be converted to db
+        # Button state is set to normal after csv file is chosen and can be converted to db
         self.convert_button["state"] = DISABLED
 
         # Adding a horizontal scrollbar to Text widget, where csv file content is displayed.
-        # Wrapping it all in a frame for better looks.
+        # Wrapping it all in a Frame for better looks.
         content_frame = Frame(tk_window, bd=2, relief=SOLID)
         content_frame.grid(column=0, row=2, columnspan=15, rowspan=15, padx=(5, 0))
         x_scrollbar = Scrollbar(content_frame, orient=HORIZONTAL)
@@ -50,38 +50,41 @@ class Client(object):
                                      "- .csv file must be clean, without additional content.  \n" \
                                      "- make sure .csv file only includes columns and rows. \n" \
                                      "- specify a name for database before converting. \n" \
-                                     "- to open .db file use software as HeidiSQL, SQLite etc.\n" \
-                                     "- choose a .csv file to see its preview in this field.\n" \
+                                     "- to open .db file use software such as SQLite Studio.\n" \
+                                     "- choose a .csv file to see a preview of it in this field.\n" \
                                      "- empty .csv files won't be converted"
 
-        self.set_preview_field(self.important_information)
+        self.reset_preview_field(self.important_information)
         x_scrollbar.config(command=self.csv_preview.xview)
 
     def import_csv_file(self):
         # Method for importing .csv file, assigning it to pandas DataFrame. And displaying it to
-        # Text widget using display_dataframe method.
+        # Text widget using display_file_content method.
         try:
             csv_file_path = filedialog.askopenfilename(title="Select a .csv file", filetypes=(("CSV Files", "*.csv"),))
             self.df_data = pd.read_csv(csv_file_path)
-            self.display_dataframe(self.df_data)
+            self.display_file_content(self.df_data)
             self.convert_button["state"] = NORMAL
             return self.df_data
         except FileExistsError:
             messagebox.showerror("File Do Not Exit", "Unable to find requested file. Please check if file exists.")
+            self.reset_preview_field(self.important_information)
         except FileNotFoundError:
             messagebox.showerror("File Not Found", "File could not be opened. Choose a valid .csv type file.")
+            self.reset_preview_field(self.important_information)
         except EmptyDataError:
             messagebox.showerror("Empty File", "No columns to parse from file")
+            self.reset_preview_field(self.important_information)
 
-    def display_dataframe(self, df_data):
-        # A method that displays a preview of imported .csv file content and
-        #  calculates all rows and columns in file.
+    def display_file_content(self, df_data):
+        # A method that displays a preview of imported .csv file content with
+        #  total numbers of all rows and columns in file.
         content_to_display = "Preview: \n \n {} \n \n Total Columns:{} \n Total Rows: {}".format(
             df_data.head().to_string(),
             len(df_data.columns),
             len(df_data)
         )
-        self.set_preview_field(content_to_display)
+        self.reset_preview_field(content_to_display)
 
     def create_database(self):
         # Creates a database from imported csv file with given name.
@@ -96,13 +99,14 @@ class Client(object):
             self.Base.metadata.create_all(engine)
             self.df_data.to_sql(self.db_name.get(), con=engine, index=True, if_exists='replace')
             self.db_name_field.delete(0, END)
-            self.set_preview_field(self.important_information)
+            self.reset_preview_field(self.important_information)
             self.convert_button['state'] = DISABLED
             info = "Database was converted successfully! To open database use an appropriate" \
                    " database managing tool, such as SQLite Studio, HeidiSQL or MySQL Workbench."
             messagebox.showinfo(title='Converted Successfully!', message=info)
 
-    def set_preview_field(self, content):
+    def reset_preview_field(self, content):
+        # Method for resetting preview field and displaying another content in it.
         self.csv_preview.config(state=NORMAL)
         self.csv_preview.delete(1.0, END)
         self.csv_preview.insert(INSERT, content)
